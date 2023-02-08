5f1084e3e3bebc3e2cf318c0ce6348fd997aae06
petrutism <118567320+petrutism@users.noreply.github.com>
2023-02-08 20:13:29 +0200

    user exists check added

--- src/main/java/lt/code/academy/InputPerson.java
+++ src/main/java/lt/code/academy/InputPerson.java
@@ -10,6 +10,7 @@ public class InputPerson {
     String personalNumber;
 
     public InputPerson(ReadFile readFile) {
+        boolean userNotExist = true;
         Scanner sc = new Scanner(System.in);
         System.out.println("Input name: ");
         name = sc.nextLine();
@@ -17,12 +18,19 @@ public class InputPerson {
         surname = sc.nextLine();
         System.out.println("Input personal number: ");
         personalNumber = sc.nextLine();
-        for(int i=0; i < readFile.persons.size(); i++){
-
+        for (int i = 0; i < readFile.persons.size(); i++) {
+            if (name.equals(readFile.persons.get(i).name()) &&
+                    surname.equals(readFile.persons.get(i).surname()) &&
+                    personalNumber.equals(readFile.persons.get(i).personalNumber())) {
+                userNotExist = false;
+            }
+        }
+        if (userNotExist) {
+            readFile.addPerson(new Person(name, surname, personalNumber));
+            System.out.println("Person successfully added...");
+        } else{
+            System.out.println("User already exist...");
         }
 
-        readFile.addPerson(new Person(name, surname, personalNumber));
-
-        System.out.println("Person successfully added...");
     }
 }
